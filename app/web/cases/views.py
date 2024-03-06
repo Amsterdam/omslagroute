0c716d2439719fc1faa10453b5fc0189613c9134
Nino van der Laan <n.van.der.laan@amsterdam.nl>
2024-03-06 15:21:21 +0100

    download case file

--- app/web/cases/views.py
+++ app/web/cases/views.py
@@ -35,6 +35,7 @@ from datetime import datetime
 from web.users.utils import *
 from web.users.utils import get_zorginstelling_medewerkers_email_list
 from operator import or_
+from django.http.response import HttpResponse
 
 logger = logging.getLogger(__name__)
 
@@ -1296,7 +1297,10 @@ def download_document(request, case_pk, document_pk):
     if not default_storage.exists(default_storage.generate_filename(document.uploaded_file.name)):
         raise Http404()
 
-    return HttpResponseRedirect(document.uploaded_file.url)
+    with default_storage.open(document.uploaded_file.name, 'rb') as file:
+        response = HttpResponse(file.read(), content_type='application/octet-stream')
+        response['Content-Disposition'] = f'attachment; filename="{document.uploaded_file.name}"'
+        return response
 
 
 
