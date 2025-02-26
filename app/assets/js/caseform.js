379847f900e740530a6d3697806c8c588d31a982
Remy van der Wereld <r.van.der.wereld@amsterdam.nl>
2025-02-26 12:45:35 +0100

    Refactor caseform.js for improved readability and consistency

--- app/assets/js/caseform.js
+++ app/assets/js/caseform.js
@@ -1,21 +1,21 @@
-function setPartnerFieldsVisible(isVisible){
-    var elems = document.getElementsByClassName('partner_form_elem');
-    var disp = isVisible?"block":"none";
-    for(var e in elems){
-        if(elems[e].parentNode){
-            elems[e].parentNode.setAttribute("style", "display:"+disp);
-        }
+function setPartnerFieldsVisible(isVisible) {
+  var elems = document.getElementsByClassName("partner_form_elem");
+  var disp = isVisible ? "block" : "none";
+  for (var e in elems) {
+    if (elems[e].parentNode) {
+      elems[e].parentNode.setAttribute("style", "display:" + disp);
     }
-}
-function updateCaseForm(elem){
-    setPartnerFieldsVisible(elem.value == 1);
+  }
 }
 
-
-window.onload = function() {
-    var partnerCheck = document.getElementById('id_partner_check');
-    partnerCheck.addEventListener("change", function () {
-        updateCaseForm(this);
-    });
-    setPartnerFieldsVisible(partnerCheck.value == 1);
+function updateCaseForm(elem) {
+  setPartnerFieldsVisible(elem.value == 1);
 }
+
+window.onload = function () {
+  var partnerCheck = document.getElementById("id_partner_check");
+  partnerCheck.addEventListener("change", function () {
+    updateCaseForm(this);
+  });
+  setPartnerFieldsVisible(partnerCheck.value == 1);
+};
