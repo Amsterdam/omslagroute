b0e17e21feb17a57ac33e15ffa647d24ae42a205
Nino <n.van.der.laan@amsterdam.nl>
2025-12-04 11:19:33 +0100

    Add export action users including roles

--- app/web/users/admin.py
+++ app/web/users/admin.py
@@ -1,12 +1,44 @@
 from django.contrib import admin, messages
 from django.contrib.auth.admin import UserAdmin
+from django.http import HttpResponse
 from .models import User
 from django.utils.translation import gettext_lazy as _
 from django.utils.html import mark_safe
 from django.urls import reverse
 from .forms import ChangeFederationForm
 from django.shortcuts import render
+from django.utils import timezone
+from openpyxl import Workbook
 
+
+@admin.action(description="Geselecteerde gebruikers exporteren inclusief rollen")
+def export_selected_users_to_excel(modeladmin, request, queryset):
+    wb = Workbook()
+    wb_sheet = wb.active
+    wb_sheet.title = "Gebruikers inclusief rollen"
+
+    headers = ["Naam", "E-mailadres", "Rollen", "Federatie", "Datum account aangemaakt", "Laatste login", "Actief"]
+    wb_sheet.append(headers)
+
+    for u in queryset.prefetch_related("groups"):
+        date_joined = timezone.localtime(u.date_joined).strftime("%d-%m-%Y %H:%M:%S") if u.date_joined else ""
+        last_login = timezone.localtime(u.last_login).strftime("%d-%m-%Y %H:%M:%S") if u.last_login else ""
+        wb_sheet.append([
+            u.name,
+            u.email,
+            u.user_type_names,
+            u.federation.name if u.federation else "",
+            date_joined,
+            last_login,
+            "Ja" if u.is_active else "Nee",
+        ])
+
+    response = HttpResponse(
+        content_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    )
+    response["Content-Disposition"] = 'attachment; filename="gebruikers_rollen.xlsx"'
+    wb.save(response)
+    return response
 
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
@@ -45,7 +77,7 @@ class CustomUserAdmin(UserAdmin):
         (_("Important dates"), {"fields": ("last_login", "date_joined")}),
     )
     list_filter = ("user_type", "federation", "is_staff", "is_superuser", "is_active")
-    actions = ["change_federation_action"]
+    actions = ["change_federation_action", export_selected_users_to_excel]
 
     def profile_link(self, obj):
         url = reverse(
